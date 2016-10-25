048260dcac99969fd19294f618083df7928db8af
Yashwanth Bushetty <Yashwanth.Bushetty@wnco.com>
2016-10-24 22:37:31 -0500

    Adding .gitignore and Pushing some more code!

---
+++ area.py
@@ -0,0 +1,10 @@
+#!/bin/usr/python3
+
+
+def area(x, y=3.14):
+    a = y*x*x
+    print(a)
+    return a
+
+a=area(10)
+print("area", a)
--- dataTypes.py
+++ dataTypes.py
@@ -4,3 +4,8 @@ x = int(input("enter a number : "))
 y = int(input("enter another number to do some action :"))
 
 print("The sum of two given numbers is: ", x+y)
+print("The modulus of the given two numbers is: ", x%y)
+print("The subtraction of the given two numbers is: ", x-y)
+print("The division of the given two numbers is: ", x/y)
+print("The multiplication of the given two numbers is: ", x*y)
+
